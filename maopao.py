6fd018ffc3f02c6308329a3cb9421ece505137d5
JKM\liuqian <liuqian@gmail.com>
2017-12-02 17:31:20 +0800

    a,b = b,a 两位置交换

--- maopao.py
+++ maopao.py
@@ -2,8 +2,8 @@ http://pythontutor.com/visualize.html#mode=edit
 heigh = [9,8,7,6,5,4]
 for j in range(0,len(heigh)-1):
     for i in range(0,len(heigh)-1):
-          if heigh[i] > heigh[i+1]:
-                heigh[i],heigh[i+1] = heigh[i+1],heigh[i]
+          if heigh[i] > heigh[i+1]:                       
+                heigh[i],heigh[i+1] = heigh[i+1],heigh[i] // a,b = b,a 两位置交换
 print heigh
 
 heigh = [9,8,7,6,5,4]
